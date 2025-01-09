26457498f3875ecbfa9b61f12f2a3bb7ab2d7f25
breixosenrapastoriza <142690734+breixosenrapastoriza@users.noreply.github.com>
2025-01-09 21:24:09 +0100

    Delete jest.config.cjs

--- jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  roots: ["<rootDir>/test"],
-  testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
-  transform: {
-    "^.+\\.[t|j]sx?$": "babel-jest",  
-  },
-};
